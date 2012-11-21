1901a9b0967998199639d802e7a3572a637f53ab
Adam <adam@novoda.com>
2012-11-21 13:03:25 +0000

    fixes #30 - removed setting package name on pendingIntent, packagename can no longer be enforced on 4.2, apps should use the manifest option export=false on their location recievers

--- core/src/main/java/com/novoda/location/LocationUpdatesIntentFactory.java
+++ core/src/main/java/com/novoda/location/LocationUpdatesIntentFactory.java
@@ -1,12 +1,11 @@
 package com.novoda.location;
 
 import android.app.PendingIntent;
-
-import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
-
 import android.content.Context;
 import android.content.Intent;
 import com.novoda.location.receiver.PassiveLocationChanged;
+
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 
 class LocationUpdatesIntentFactory {
 
@@ -20,13 +19,11 @@ class LocationUpdatesIntentFactory {
 
     PendingIntent buildActive() {
         Intent activeIntent = new Intent(Constants.ACTIVE_LOCATION_UPDATE_ACTION);
-        activeIntent.setPackage(settings.getPackageName());
         return PendingIntent.getBroadcast(context, 0, activeIntent, FLAG_UPDATE_CURRENT);
     }
 
     PendingIntent buildPassive() {
         Intent passiveIntent = new Intent(context, PassiveLocationChanged.class);
-        passiveIntent.setPackage(settings.getPackageName());
         return PendingIntent.getBroadcast(context, 0, passiveIntent, FLAG_UPDATE_CURRENT);
     }
 
