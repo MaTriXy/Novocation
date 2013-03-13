f7aa448ffe833d34d2a47e116030b13d1e9c1959
malmstein <malmstein@gmail.com>
2013-03-13 17:19:41 +0000

    updated google play services to 5

--- demo/src/com/novoda/locationdemo/fragment/DemoSupportMapFragment.java
+++ demo/src/com/novoda/locationdemo/fragment/DemoSupportMapFragment.java
@@ -7,10 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
